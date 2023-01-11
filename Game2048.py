7fe16d2b44dd9fbca450bee5edc12c37b6993262
marc maunier <marc_jerome@hotmail.com>
2023-01-11 14:33:46 +0100

    Game variable initialize

--- Game2048.py
+++ Game2048.py
@@ -1,11 +1,11 @@
 
-# Build 2048 in python using pygame
+#--Build 2048 in python using pygame
 import pygame
 import random
 
 pygame.init()
 
-# Initial set up
+#--Initial set up
 WIDTH = 400
 HEIGHT = 500
 screan = pygame.display.set_mode([WIDTH, HEIGHT])
@@ -13,5 +13,25 @@ pygame.display.set_caption('2048')
 timer = pygame.time.Clock()
 fps = 60
 font = pygame.font.Font('freesambold.ttf', 24)
+
+#--2048 game colo library
+color = {0: (204, 192, 179),
+         2: (238, 228, 218),
+         4: (237, 224, 200),
+         8: (242, 177, 121),
+         16: (245, 149, 99),
+         32: (246, 124, 95),
+         64: (246, 94, 59),
+         128: (237, 207, 114),
+         256: (237, 204, 97),
+         512: (237, 200, 80),
+         1024: (237, 197, 63),
+         2048: (237, 194, 46),
+         'light text' : (249, 246, 242),
+         'dark tex' : (119, 110, 101),
+         'other' : (0, 0, 0,),
+         'bg' : (187, 173, 160)}
+
+#--Game variable initialize
 
 
