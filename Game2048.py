d9525ff3a91e9065c2431c5b695711a24a2f9f91
marc maunier <marc_jerome@hotmail.com>
2023-01-11 16:00:42 +0100

    Moving down

--- Game2048.py
+++ Game2048.py
@@ -53,3 +53,24 @@ def draw_over():
     screen.blit(game_over_text2, (70, 105))
     
 #--Take your turn based on direction
+def take_turn(direc, board):
+    global score
+    merged = [[False for _ in range(4)] for _ in range(4)]
+    if direc == 'UP':
+        for i in range(4):
+            for j in range(4):
+                shift = 0
+                if i > 0:
+                    for q in range(i):
+                        if board[q][j] == 0:
+                            shift += 1
+                        if shift > 0:
+                            board[i - shift][j] = board[i][j]
+                            board[i][j] = 0
+                        if board[i - shift - 1][j] == board[i - shift][j] and not merged[i - shift - 1][j] \
+                            and not merged[i - shift - 1][j]:
+                            board[i - shift - 1][j] *= 2
+                            score += board[i - shift - 1][j]
+                            board[i - shift][j] = 0
+                            merged[i - shift - 1][j] = True
+                            
