48265ec97820fb00e91a06614379cdfacc6f5f73
Marc Jerome <marcjerome@MacBook-Pro-de-Marc.local>
2023-01-11 12:53:34 +0100

    Initial set up

--- Game2048.py
+++ Game2048.py
@@ -1,1 +1,18 @@
 
+# Build 2048 in python using pygame
+import pygame
+import random
+
+pygame.init()
+
+# Initial set up
+WIDTH = 400
+HEIGHT = 500
+screan = pygame.display.set_mode([WIDTH, HEIGHT])
+pygame.display.set_caption('2048')
+timer = pygame.time.Clock()
+fps = 60
+font = pygame.font.Font('freesambold.ttf', 24)
+
+
+
